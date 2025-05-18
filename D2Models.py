ca8fc76f1e8004d00dd38a49b68bd6283e5aab98
Faseeh-Ops <fashiali412@gmail.com>
2025-05-18 19:23:18 +0500

    Define Train Test paths

--- D2Models.py
+++ D2Models.py
@@ -1,6 +1,5 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
 def train_and_evaluate(X_train, X_test, y_train, y_test):
     model = LogisticRegression(max_iter=200)
     model.fit(X_train, y_train)
