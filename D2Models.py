e1a778cd4994e8197f05894aea55d1ba0fb01a59
Faseeh-Ops <fashiali412@gmail.com>
2025-05-19 20:21:04 +0500

    Use model Logistic Regression on second dataset

--- D2Models.py
+++ D2Models.py
@@ -5,4 +5,5 @@ def train_and_evaluate(X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
     acc = accuracy_score(y_test, predictions)
+
     print(f"Model Accuracy: {acc:.2f}")
