0679fcc18fd5dc13c09ddfe51dde9c76a0b08a1f
Faseeh-Ops <fashiali412@gmail.com>
2025-05-23 22:57:10 +0500

    Use LogisticRegression for model training

--- D2Models.py
+++ D2Models.py
@@ -1,9 +1,7 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-def train_and_evaluate(X_train, X_test, y_train, y_test):
+
+def train_and_evaluate(X_train, y_train):
+
     model = LogisticRegression(max_iter=200)
     model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-    acc = accuracy_score(y_test, predictions)
-
-    print(f"Model Accuracy: {acc:.2f}")
+    return model
